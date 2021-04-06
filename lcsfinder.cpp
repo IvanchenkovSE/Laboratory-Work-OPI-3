a03525cc73ffa3618ff86d058dd5e5b304391d46
Dmitry Andreev <Coolich.ya@gmail.com>
2021-04-06 20:39:50 +0300

    Updated variable names

--- lcsfinder.cpp
+++ lcsfinder.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
 
-
-
-int max(int a, int b);
+int max(int lenOfFirstLine, int lenOfSecondLine);
 
 
 /* Возвращает длину LCS для X[0..m - 1], Y[0..n - 1] */
 
-int lcs(char* X, char* Y, int m, int n)
+int lcs(char* firstLine, char* secondLine, int sizeOfFirstLine, int sizeOfSecondLine)
 
 {
 
-    if (m == 0 || n == 0)
+    if (sizeOfFirstLine == 0 || sizeOfSecondLine == 0)
 
         return 0;
 
-    if (X[m - 1] == Y[n - 1])
+    if (firstLine[sizeOfFirstLine - 1] == secondLine[sizeOfSecondLine - 1])
 
-        return 1 + lcs(X, Y, m - 1, n - 1);
+        return 1 + lcs(firstLine, secondLine, sizeOfFirstLine - 1, sizeOfSecondLine - 1);
 
     else
 
-        return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
+        return max(lcs(firstLine, secondLine, sizeOfFirstLine, sizeOfSecondLine - 1), lcs(firstLine, secondLine, sizeOfFirstLine - 1, sizeOfSecondLine));
 
 }
 
 
 /*Функция полезности для получения максимум 2 целых чисел */
 
-int max(int a, int b)
-
+int max(int lenOfFirstLine, int lenOfSecondLine)
 {
-
-    return (a > b) ? a : b;
-
+    return (lenOfFirstLine > lenOfSecondLine) ? lenOfFirstLine : lenOfSecondLine;
 }
 
 
@@ -48,15 +43,12 @@ int main()
     char Y[] = "GXTXAYBI";
 
 
-
     int m = strlen(X);
 
     int n = strlen(Y);
 
 
-
     printf("Length of LCS is %d\n", lcs(X, Y, m, n));
-
 
 
     return 0;
