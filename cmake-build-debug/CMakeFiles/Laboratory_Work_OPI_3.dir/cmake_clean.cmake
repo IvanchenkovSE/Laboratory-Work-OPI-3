e1aa2ce7d09ca7fc67a0117579a3e7b06d73e7a8
Dmitry Andreev <Coolich.ya@gmail.com>
2021-04-06 20:57:09 +0300

    Updated variable names

---
+++ cmake-build-debug/CMakeFiles/Laboratory_Work_OPI_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratory_Work_OPI_3.dir/lcsfinder.cpp.obj"
+  "Laboratory_Work_OPI_3.exe"
+  "Laboratory_Work_OPI_3.exe.manifest"
+  "Laboratory_Work_OPI_3.lib"
+  "Laboratory_Work_OPI_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laboratory_Work_OPI_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
